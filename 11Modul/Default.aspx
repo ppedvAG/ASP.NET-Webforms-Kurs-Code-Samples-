45f6cb9126aca7ab47f52684e0f5a78d48d73ef7
bill gates <hannesp@ppedv.de>
2017-06-02 11:11:12 +0200

    Cache Objekt

---
+++ 11Modul/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_11Modul_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContentPlaceholder" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+    <div class="row">
+                 <div class="col-md-8">
+                    <ul class="list-group">
+
+                        <asp:Repeater ID="rptListe" runat="server" EnableViewState="false"
+                            ItemType="todo"
+                            SelectMethod="rptListe_GetData">
+                            <ItemTemplate>
+                                <li class="list-group-item">
+                                    <div class="row">
+                                        <div class="col-md-2"><%#Item.ID%></div>
+                                        <div class="col-md-6"><%#Item.Aufgabe %></div>
+                                    </div>
+                                </li>
+                            </ItemTemplate>
+                        </asp:Repeater>
+
+                    </ul>
+
+                </div>
+
+    </div>
+</asp:Content>
+
---
+++ 11Modul/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _11Modul_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+       
+
+    }
+
+    public IEnumerable<todo> rptListe_GetData()
+    {
+        var todoList = new List<todo>();
+        if (Cache["todos"] != null)
+        {
+            todoList = (List<todo>)Cache["todos"];
+
+        }
+        else
+        {
+
+            try
+            {
+                var items = File.ReadAllLines(Server.MapPath(@"~\app_data\todoitems.txt"));
+                int id = 0;
+                foreach (string s in items)
+                {
+                    todoList.Add(new todo() { ID = id, Aufgabe = s });
+                    id++;
+
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+        //Cache.Add("todos", todoList, 
+        //    new System.Web.Caching.CacheDependency(Server.MapPath(@"~\app_data\todoitems.txt")),
+        //   DateTime.Now.AddMinutes(3),
+        //    new TimeSpan(0, 3, 0),
+        //    System.Web.Caching.CacheItemPriority.Default,
+        //    null);
+        Cache["todos"] = todoList;
+
+
+
+        return todoList;
+    }
+
+}
