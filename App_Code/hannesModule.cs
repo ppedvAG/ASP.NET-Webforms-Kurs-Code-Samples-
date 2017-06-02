a33685742ca2113cf60e4c4bc429f44b4cee0d61
bill gates <hannesp@ppedv.de>
2017-06-02 14:27:56 +0200

    Modul Idee

---
+++ App_Code/hannesModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Zusammenfassungsbeschreibung für hannesModule
+/// </summary>
+public class hannesModule:IHttpModule
+{
+    public hannesModule()
+    {
+        //
+        //TODO: Hier Konstruktorlogik hinzufügen
+        //
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Init(HttpApplication context)
+    {
+        context.Response.ContentType = "text/plain";
+        context.Response.Write("(c) pppedv");
+    }
+}
