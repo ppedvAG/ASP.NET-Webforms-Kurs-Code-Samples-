a50e5ddc9a5c54cc604add87e7e10a7aa325429e
bill gates <hannesp@ppedv.de>
2017-06-02 11:44:36 +0200

    Excel Server manuel

---
+++ 12Modul/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_12Modul_Default" %>
+
+<table>
+    <asp:repeater id="rptListe" runat="server" enableviewstate="false"
+        itemtype="todo"
+        selectmethod="rptListe_GetData">
+                            <ItemTemplate>
+                                <tr><td>
+                               <%#Item.ID%>
+
+                                    </td>
+                                <td
+                                    <%#Item.Aufgabe %></td>
+
+
+                                </tr>
+                            </ItemTemplate>
+                        </asp:repeater>
+</table>
---
+++ 12Modul/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _12Modul_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.ContentType = "application/vnd.ms-excel";
+
+
+    }
+
+    public IEnumerable<todo> rptListe_GetData()
+    {
+        var todoList = new List<todo>();
+        try
+        {
+            var items = File.ReadAllLines(Server.MapPath(@"~\app_data\todoitems.txt"));
+            int id = 0;
+            foreach (string s in items)
+            {
+                todoList.Add(new todo() { ID = id, Aufgabe = s });
+                id++;
+
+            }
+        }
+        catch
+        {
+
+        }
+
+        return todoList;
+    }
+}
